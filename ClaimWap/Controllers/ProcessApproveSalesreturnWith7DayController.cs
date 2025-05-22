366eaf1645e0533e194b211751fcb1238b67fd7e
warakornaac <warakorn.pra@aac.co.th>
2025-05-22 10:52:45 +0700

    [controller] add new controller

---
+++ ClaimWap/Controllers/ProcessApproveSalesreturnWith7DayController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClaimWap.Controllers
+{
+    public class ProcessApproveSalesreturnWith7DayController : Controller
+    {
+        //
+        // GET: /ProcessApproveSalesreturnWith7Day/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
